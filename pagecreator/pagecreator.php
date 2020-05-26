706dc42cb9dfa02e710b3aa9792ff50fd5a85c1a
Lysander Mealy <lysander.mealy@gmail.com>
2020-05-26 10:45:10 -0400

    Commit

---
+++ pagecreator/pagecreator.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Creator</title>
+</head>
+
+<body>
+    <?php
+    for ($i = 0; $i < 10; $i++) {
+        $id .= mt_rand(0, 9);
+    }
+    $openDocument = fopen("./Documents/" . $id . ".php", "w");
+    echo '<iframe src="./Documents/' . $id . '.php' . '" frameborder="0"></iframe>';
+    ?>
+</body>
+
+</html>
